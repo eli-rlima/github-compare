1600ba6a8fe2e4a4a23c5ac94bacad0871ee3c7b
eli_rlima <elivelton@salvus.me>
2021-02-07 17:09:18 -0300

    Changes to remove dependecies of app to navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,22 @@
 // Global
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useDebounce } from 'use-debounce';
+import React from 'react';
+import { useSelector } from 'react-redux';
 // Components
 import Navbar from 'components/Navbar';
 import EmptyState from 'components/EmptyState';
 import NewRepoPopup from 'components/NewRepoPopup';
 import Grid from 'components/Grid';
 import DeleteModal from 'components/DeleteModal';
-// Actions
-import { show } from 'redux/actions/popup.action';
-import { 
-  search as searchAction,
-  orderBy,
-  favoriteFilter as favoriteFilterAction, 
-} from 'redux/actions/repositories.action';
+
 // Stylesheet
 import './App.scss';
 
 function App() {
-  const [search, setSearch] = useState('');
-  const [favoriteFilter, setFavoriteFilter] = useState(false);
-  const dispatch = useDispatch();
-  const { isOpened } = useSelector(state => state.popup);
-  const { filter, data } = useSelector(state => state.repositories);
-  const { data: dataFilter } = filter;
-
-  const onChange = (e, target) => {
-    const { value } = target || e.target;
-
-    setSearch(value)
-    if (data.length > 0) {
-      dispatch(searchAction(value));
-    }
-  }
-
-  const onClickOrder = key => {
-    dispatch(orderBy(key));
-  }
-
-  const onClickFavoriteFilter = () => {
-    setFavoriteFilter(!favoriteFilter);
-    dispatch(favoriteFilterAction(!favoriteFilter));
-  }
-
-  const clearSearch = () => {
-    if (data.length === 0) {
-      setSearch('');
-    }
-  }
-
-  useDebounce(clearSearch, 300);
-
-  const showPopup = action => {
-    dispatch(show(action));
-  }
+  const { data: dataFilter } = useSelector(state => state.repositories.filter);
 
   return (
     <div className='App'>
-      <Navbar 
-        onClick={() => showPopup(!isOpened)}
-        onClickOrder={onClickOrder}
-        onChange={onChange}
-        onClickFavoriteFilter={onClickFavoriteFilter}
-        value={search}
-        valueFavoriteFilter={favoriteFilter}
-      />
+      <Navbar />
       <div className='body'>
         {dataFilter.length > 0 ? <Grid data={dataFilter} as="grid" /> : <EmptyState />}
       </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,7 @@
 // Global
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useDebounce } from 'use-debounce';
 import PropTypes from 'prop-types';
 // Components
 import ButtonPlus from 'components/common/ButtonPlus';
@@ -10,18 +12,59 @@ import ButtonFavorite from 'components/common/ButtonFavorite';
 import GitHubLogo from 'assets/logo/github_logo';
 import Cards2Icon from 'assets/icons/cards2';
 import AdjustIcon from 'assets/icons/adjust'
+// Actions
+import { show } from 'redux/actions/popup.action';
+import { 
+  search as searchAction,
+  orderBy,
+  favoriteFilter as favoriteFilterAction, 
+} from 'redux/actions/repositories.action';
 // Stylesheet
 import './index.scss';
 
-function Navbar({ onClick, onChange, value, onClickOrder, onClickFavoriteFilter, valueFavoriteFilter }) {
-
+function Navbar() {
   const data = [
     { name: 'Stars', key: 'stargazers_count' },
     { name: 'Forks', key: 'forks_count' },
     { name: 'Opens Issues', key: 'open_issues' },
     { name: 'Age', key: 'created_at' },
     { name: 'Last Commit', key: 'updated_at' },
-  ]
+  ];
+  const [search, setSearch] = useState('');
+  const [favoriteFilter, setFavoriteFilter] = useState(false);
+  const dispatch = useDispatch();
+  const { isOpened } = useSelector(state => state.popup);
+  const { data: dataRepositories } = useSelector(state => state.repositories);
+
+  const onChange = (e, target) => {
+    const { value } = target || e.target;
+
+    setSearch(value)
+    if (dataRepositories.length > 0) {
+      dispatch(searchAction(value));
+    }
+  }
+
+  const onClickOrder = key => {
+    dispatch(orderBy(key));
+  }
+
+  const onClickFavoriteFilter = () => {
+    setFavoriteFilter(!favoriteFilter);
+    dispatch(favoriteFilterAction(!favoriteFilter));
+  }
+
+  const clearSearch = () => {
+    if (dataRepositories.length === 0) {
+      setSearch('');
+    }
+  }
+
+  useDebounce(clearSearch, 300);
+
+  const showPopup = action => {
+    dispatch(show(action));
+  }
 
   return (
     <div className='nav'>
@@ -39,10 +82,10 @@ function Navbar({ onClick, onChange, value, onClickOrder, onClickFavoriteFilter,
         style={{ width: '519px' }}
         placeholder="Search"
         onChange={onChange}
-        value={value}
+        value={search}
       />
       <div className='nav_star-o'>
-        <ButtonFavorite onClick={onClickFavoriteFilter} value={valueFavoriteFilter} />
+        <ButtonFavorite onClick={onClickFavoriteFilter} value={favoriteFilter} />
       </div>
       <div className='nav_adjust'>
         <AdjustIcon />
@@ -51,7 +94,7 @@ function Navbar({ onClick, onChange, value, onClickOrder, onClickFavoriteFilter,
         <Cards2Icon />
       </div>
       <div className='nav_button-add'>
-        <ButtonPlus onClick={onClick} />
+        <ButtonPlus onClick={() => showPopup(!isOpened)} />
       </div>
     </div>
   );
